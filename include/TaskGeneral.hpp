a7b0a0a23f9c983995cdfbd46d8d19d0fead8c4a
biniu <dawid.binkowski93@gmail.com>
2019-06-20 00:39:43 +0200

    Revert "add project ref to task"
    This reverts commit 5a79bf2bc961097a10ae2e552e9f682a30cc9383.

--- include/TaskGeneral.hpp
+++ include/TaskGeneral.hpp
@@ -4,16 +4,13 @@
 #define TASKGENERAL
 
 #include <chrono>
-#include <memory>
 #include <string>
-
-#include "ProjectGeneral.hpp"
 
 class TaskGeneral {
 public:
   TaskGeneral() = delete;
-  TaskGeneral(const ProjectGeneral &project_ref, const std::string &task_name);
-  TaskGeneral(const ProjectGeneral &project_ref, const std::string &task_name,
+  explicit TaskGeneral(const std::string &task_name);
+  TaskGeneral(const std::string &task_name,
               const std::string &task_description);
 
   virtual ~TaskGeneral();
@@ -27,8 +24,6 @@ public:
 private:
   std::string name{""};
   std::string description{""};
-
-  std::unique_ptr<ProjectGeneral> project;
 
   std::chrono::time_point<std::chrono::system_clock> creation_time;
 };
