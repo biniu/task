a4293b40da282aa9a76c357ad75e04442b0c42b7
biniu <dawid.binkowski93@gmail.com>
2019-06-19 19:29:32 +0200

    add basic set/get methods

--- include/ProjectGeneral.hpp
+++ include/ProjectGeneral.hpp
@@ -9,16 +9,22 @@
 class ProjectGeneral {
 public:
   ProjectGeneral() = delete;
-  ProjectGeneral(const std::string &project_name);
+  explicit ProjectGeneral(const std::string &project_name);
   ProjectGeneral(const std::string &project_name, const std::string &project_description);
 
   ProjectGeneral(const ProjectGeneral &src) = default;
 
   virtual ~ProjectGeneral();
 
+  void SetName(const std::string &project_name);
+  void SetDescription(const std::string &project_description);
+
+  std::string GetName();
+  std::string GetDescription();
+
 private:
-  std::string name;
-  std::string description;
+  std::string name{""};
+  std::string description{""};
 
   std::chrono::time_point<std::chrono::system_clock> creation_time;
 };
